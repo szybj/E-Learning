e32b653985115b934a1fa810c342ea8a6289aa01
szybj <songzhiyin@outlook.com>
2015-11-30 18:11:58 +0800

    整理

--- js/t_javaScript.js
+++ js/t_javaScript.js
@@ -1,4 +1,52 @@
 
+ function $( id ){
+    return typeof id === 'string'?document.getElementById( id ):id;
+    }
+
+    //获取class
+
+    function getElementsByClassName( node,classname ){
+    	if( node.getElementsByClassName ){
+    		//使用现有的方法
+    		return node.getElementsByClassName( classname );
+    	}else{
+    		var results = new Array();
+    		var elems = node.getElementsByTagName('*');
+    		for( var i = 0; i<elems.length; i++ ){
+    			if( elems[i].className.indexOf( classname ) != -1 ){
+    				results[results.length] = elems[i];
+    			}
+    		}
+
+    		return results;
+    	}
+    }
+
+
+function t_left(){
+	var oLeftUl = document.getElementById('t_leftul');
+	var aLeftLi = oLeftUl.getElementsByTagName('li');
+
+	for( var i = 0; i<aLeftLi.length; i++ ){
+		if( i%2 == 0 ){
+			aLeftLi[i].className = 'new_color';
+		}else{
+			aLeftLi[i].className = '';
+		}
+
+		aLeftLi[i].onmouseover = function(){
+		oldColor = this.className;
+		this.className = 'across';
+	}
+
+		aLeftLi[i].onmouseout = function(){
+			
+			this.className = oldColor;
+		}
+
+	}
+
+}
 
 
 function t_top(){
@@ -21,6 +69,7 @@ function t_top(){
 		},500)
 	}
 
+	
 
 	//鼠标滑过改变颜色
 	var oUl = document.getElementById('t_list').getElementsByTagName('ul')[0];
@@ -48,30 +97,6 @@ function t_top(){
 }
 
 
-	function t_left(){
-	var oLeftUl = document.getElementById('t_leftul');
-	var aLeftLi = oLeftUl.getElementsByTagName('li');
-
-	for( var i = 0; i<aLeftLi.length; i++ ){
-		if( i%2 == 0 ){
-			aLeftLi[i].className = 'new_color';
-		}else{
-			aLeftLi[i].className = '';
-		}
-
-		aLeftLi[i].onmouseover = function(){
-		oldColor = this.className;
-		this.className = 'across';
-	}
-
-		aLeftLi[i].onmouseout = function(){
-			
-			this.className = oldColor;
-		}
-
-	}
-
-}
 
 
 function t_onlineLessons(){
@@ -205,3 +230,215 @@ for( var i = 0; i<aLayerThickness.length; i++ ){
 var aPlayTitle = getElementsByClassName( boxSet,'playTitle' );
 alert(aPlayTitle .length);*/
 
+
+ //自动居中 - 登录浮层( el = Element )
+    function autoCenter( el ){
+
+        //获取页面可视区的宽高
+        var wHeight = document.documentElement.clientHeight;
+        var wWidth= document.documentElement.clientWidth;
+
+        //获取弹出框的宽和高
+        var dHeight = el.offsetHeight;
+        var dWidth = el.offsetWidth;
+
+        el.style.top = ( wHeight-dHeight )/2 + 'px';
+        el.style.left = ( wWidth-dWidth )/2 + 'px';
+
+    }
+
+    //自动全屏 - 遮罩
+    function fillToBody( el ){
+        el.style.height = document.documentElement.scrollHeight+ 'px';
+        el.style.width= document.documentElement.scrollWidth + 'px';
+    }
+
+    var mouseOffsetX = 0;   //偏移
+    var mouseOffsetY = 0;
+
+    var isDraging = false; //标记是否拖拽
+
+    //鼠标事件1-在标题栏上按下( 要计算鼠标相对拖拽元素的左上角坐标，并且标记元素为可拖动)
+
+    $('contentTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetY = e.pageY - $('showContent').offsetTop;
+        mouseOffsetX = e.pageX - $('showContent').offsetLeft;
+        isDraging = true;
+
+    })
+
+    $('videoTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetY = e.pageY - $('showVideo').offsetTop;
+        mouseOffsetX = e.pageX - $('showVideo').offsetLeft;
+        isDraging = true;
+
+    })
+
+    $('dataTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetY = e.pageY - $('showData').offsetTop;
+        mouseOffsetX = e.pageX - $('showData').offsetLeft;
+        isDraging = true;
+
+    })
+
+    $('homeworkTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetY = e.pageY - $('showHomework').offsetTop;
+        mouseOffsetX = e.pageX - $('showHomework').offsetLeft;
+        isDraging = true;
+
+    })
+
+    $('testTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetY = e.pageY - $('showTest').offsetTop;
+        mouseOffsetX = e.pageX - $('showTest').offsetLeft;
+        isDraging = true;
+
+    })
+
+    $('releaseTitle').addEventListener('mousedown',function(e){
+        var e = e || window.event;
+        mouseOffsetY = e.pageY - $('showRelease').offsetTop;
+        mouseOffsetX = e.pageX - $('showRelease').offsetLeft;
+        isDraging = true;
+
+    })
+    //鼠标事件2-鼠标移动时( 要检测元素是否可标记为移动，如果是，则更新元素位置到当前鼠标的位置【要减去第一步的偏移量】)
+    document.onmousemove = function( e ){
+        var e = e||window.event;
+        
+        var mouseX = e.pageX;     //鼠标当前的位置
+        var mouseY = e.pageY;
+
+        var moveX = 0;    //浮层元素的新位置
+        var moveY = 0;  
+
+        if( isDraging === true ){
+            moveX = mouseX - mouseOffsetX;
+            moveY = mouseY - mouseOffsetY;
+
+            //范围限定 moveX > 0 并且 moveX < (页面最大宽度 - 浮层的宽度)
+            //范围限定 moveY > 0 并且 moveY < (页面最大高度 - 浮层的高度)
+
+            var wHeight = document.documentElement.clientHeight;
+            var wWidth= document.documentElement.clientWidth;
+
+            //获取弹出框的宽和高
+            var dHeight = $('showContent').offsetHeight;
+            var dWidth = $('showContent').offsetWidth;
+
+           
+           /* var dHeight = $('showVideo').offsetHeight;
+            var dWidth = $('showVideo').offsetWidth;
+
+            var dHeight = $('showData').offsetHeight;
+            var dWidth = $('showData').offsetWidth;
+
+            var dHeight = $('showHomework').offsetHeight;
+            var dWidth = $('showHomework').offsetWidth;
+
+            var dHeight = $('showTest').offsetHeight;
+            var dWidth = $('showTest').offsetWidth;
+
+            var dHeight = $('showRelease').offsetHeight;
+            var dWidth = $('showRelease').offsetWidth;
+*/
+       
+            var  maxY= wHeight - dHeight;
+            var maxX= wWidth - dWidth;
+
+            moveX = Math.min( maxX,Math.max(0,moveX));
+            moveY = Math.min( maxY,Math.max(0,moveY));
+
+            $('showContent').style.left = moveX + 'px';
+            $('showContent').style.top = moveY + 'px';
+
+            $('showVideo').style.left = moveX + 'px';
+            $('showVideo').style.top = moveY + 'px';
+
+            $('showData').style.left = moveX + 'px';
+            $('showData').style.top = moveY + 'px';
+
+            $('showHomework').style.left = moveX + 'px';
+            $('showHomework').style.top = moveY + 'px';
+
+            $('showTest').style.left = moveX + 'px';
+            $('showTest').style.top = moveY + 'px';
+
+            $('showRelease').style.left = moveX + 'px';
+            $('showRelease').style.top = moveY + 'px';
+
+        }
+    }
+    //鼠标事件3-鼠标松开的时候( 标记元素为不可拖动即可)
+    document.onmouseup = function(){
+        isDraging = false;
+
+    }
+
+function showContent(){
+    $('showContent').style.display = 'block';
+    $('mask').style.display = 'block';
+    autoCenter( $('showContent') );
+    fillToBody( $('mask') );
+}
+function showVideo(){
+    $('showVideo').style.display = 'block';
+    $('mask').style.display = 'block';
+    autoCenter( $('showVideo') );
+    fillToBody( $('mask') );
+}
+
+function showData(){
+    $('showData').style.display = 'block';
+    $('mask').style.display = 'block';
+    autoCenter( $('showData') );
+    fillToBody( $('mask') );
+}
+
+function showHomework(){
+    $('showHomework').style.display = 'block';
+    $('mask').style.display = 'block';
+    autoCenter( $('showHomework') );
+    fillToBody( $('mask') );
+}
+
+function showTest(){
+    $('showTest').style.display = 'block';tiptop
+    $('mask').style.display = 'block';
+    autoCenter( $('showTest') );
+    fillToBody( $('mask') );
+}
+
+function showRelease(){
+    $('showRelease').style.display = 'block';
+    $('mask').style.display = 'block';
+    autoCenter( $('showRelease') );
+    fillToBody( $('mask') );
+}
+
+
+function hidden(){
+    $('showContent').style.display = 'none';
+    $('showVideo').style.display = 'none';
+    $('showData').style.display = 'none';
+    $('showHomework').style.display = 'none';
+    $('showTest').style.display = 'none';
+    $('showRelease').style.display = 'none';
+    $('mask').style.display = 'none';
+}
+
+
+window.onresize = function(){
+    autoCenter( $('showContent') );
+    autoCenter( $('showVideo') );
+    autoCenter( $('showData') );
+    autoCenter( $('showHomework') );
+    autoCenter( $('showTest') );
+    autoCenter( $('showRelease') );
+    fillToBody( $('mask') );
+}
